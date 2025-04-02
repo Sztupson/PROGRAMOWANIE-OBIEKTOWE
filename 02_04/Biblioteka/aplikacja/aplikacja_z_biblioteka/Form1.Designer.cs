b64cd841000d81b9880f2dc0bf73881ac0d732e2
Sztupson <148426917+Sztupson@users.noreply.github.com>
2025-04-02 15:27:39 +0200

    Add files via upload
    C# - Walidacja peselu, tworzenie komponentów

---
+++ 02_04/Biblioteka/aplikacja/aplikacja_z_biblioteka/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace aplikacja_z_biblioteka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lblN_Po_K = new System.Windows.Forms.Label();
+            this.lblSilnia = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(154, 90);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // lblN_Po_K
+            // 
+            this.lblN_Po_K.AutoSize = true;
+            this.lblN_Po_K.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblN_Po_K.Location = new System.Drawing.Point(280, 191);
+            this.lblN_Po_K.Name = "lblN_Po_K";
+            this.lblN_Po_K.Size = new System.Drawing.Size(113, 25);
+            this.lblN_Po_K.TabIndex = 3;
+            this.lblN_Po_K.Text = "N_po_K = ";
+            // 
+            // lblSilnia
+            // 
+            this.lblSilnia.AutoSize = true;
+            this.lblSilnia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSilnia.Location = new System.Drawing.Point(316, 92);
+            this.lblSilnia.Name = "lblSilnia";
+            this.lblSilnia.Size = new System.Drawing.Size(42, 25);
+            this.lblSilnia.TabIndex = 4;
+            this.lblSilnia.Text = "! = ";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown2.Location = new System.Drawing.Point(154, 170);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown3.Location = new System.Drawing.Point(154, 207);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown3.TabIndex = 6;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.lblSilnia);
+            this.Controls.Add(this.lblN_Po_K);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lblN_Po_K;
+        private System.Windows.Forms.Label lblSilnia;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ 02_04/PESEL/PESEL/pesel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PESEL
+{
+    public partial class pesel: TextBox
+    {
+        public pesel()
+        {
+            InitializeComponent();
+        }
+        private Color KolorBledu;
+        private Boolean Poprawny;
+        private String Plec;
+
+
+        public Color _KolorBłędu
+        {
+            get { return KolorBledu; }
+            set { KolorBledu = value; }
+        }
+        public Boolean _Poprawny
+        {
+            get { return Poprawny; }
+            set { Poprawny = value; }
+        }
+        public String _Płeć
+        {
+            get { return Plec; }
+            set { Plec = Text; }
+
+        }
+        private void pesel_Validating(object sender, CancelEventArgs e)
+        {
+            int[] pp;
+            pp = this.Text.ToCharArray().Select(s => int.Parse(s.ToString())).ToArray();
+
+            if (this.Text.Length < 11 || this.Text.Length > 11) 
+            {
+                this.BackColor = KolorBledu;
+                this.Poprawny = false;
+                MessageBox.Show("Niepoprawna ilość cyfr w numerze PESEL.");
+            } 
+            else
+            {
+                if (pp[9] % 2 == 0)
+                { Plec = "Kobieta"; }
+                else
+                { Plec = "Mężczyzna"; }
+                this.Poprawny = true;
+            }
+        }
+    }
+}
