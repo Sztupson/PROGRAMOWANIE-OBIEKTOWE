e7f15df4f4e94b930bbeab81b89537487c45474f
Wiktor <sztupson@gmail.com>
2025-04-17 10:33:48 +0200

    dzepetto

--- Sem4/big_data/zajecia07.py
+++ Sem4/big_data/zajecia07.py
@@ -2,17 +2,25 @@ import threading
 import time
 import random
 
+mylist = []
+
 def job(n):
-    sleep_time = random.randint(1,3)
-    print(f"Worker number {n}: sleep for {sleep_time} seconds.")
-    time.sleep(sleep_time)
-    print(f"Worker number {n}: done.")
+    mylist.append(n)
+    v = 0
+    for i in range(10000):
+        v += 1
+    print(f"Worker number {n}")
+    print(len(mylist))
+    print(mylist)
 
 def test():
+    workers =[]
     begin = time.time()
 
     for i in range(10):
-        threading.Thread(target=job, args=(i,)).start()
+        worker = multiprocessing.Process(target=job,args=())
+        workers.append()
+        worker.st
 
     end = time.time()
     print("Done")
