ce620099b51ef1b3c0484d4d403cac8b96af1458
Sztupson <148426917+Sztupson@users.noreply.github.com>
2025-05-27 14:46:32 +0200

    Add files via upload

---
+++ PESEL/PESEL/pesel.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace PESEL
+{
+    partial class pesel
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // pesel
+            // 
+            this.Size = new System.Drawing.Size(104, 20);
+            this.TextChanged += new System.EventHandler(this.pesel_TextChanged);
+            this.Validating += new System.ComponentModel.CancelEventHandler(this.pesel_Validating);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
